293a476e24f8d2eae272bf36c7a7e003e29a283c
ngoc.vang.minh <ngoc.vang.minh@nextop.asia>
2019-10-12 13:19:56 +0900

    Dog door: initialize

---
+++ ObjectOrientedAnalysicAndDesign/src/test/java/DougDogDoor/DogDoorSimulator.java
@@ -0,0 +1,20 @@
+package DougDogDoor;
+
+import DougDogDoors.DogDoor;
+import DougDogDoors.Remote;
+
+public class DogDoorSimulator {
+    public static void main(String[] args) {
+        DogDoor door = new DogDoor();
+        Remote remote = new Remote(door);
+
+        System.out.println("Fido barks to go outside...");
+        remote.pressButton();
+        System.out.println("\nFido has gone outside...");
+        remote.pressButton();
+        System.out.println("\nFido's all done...");
+        remote.pressButton();
+        System.out.println("\nFido's back inside...");
+        remote.pressButton();
+    }
+}
