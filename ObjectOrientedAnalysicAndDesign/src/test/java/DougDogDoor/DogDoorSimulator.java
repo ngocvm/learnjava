f7d67955b105256153815b3024b8f02e9b433b58
ngoc.vang.minh <ngoc.vang.minh@nextop.asia>
2019-10-16 11:43:20 +0900

    Dog door: version 2.3

--- ObjectOrientedAnalysicAndDesign/src/test/java/DougDogDoor/DogDoorSimulator.java
+++ ObjectOrientedAnalysicAndDesign/src/test/java/DougDogDoor/DogDoorSimulator.java
@@ -1,15 +1,17 @@
 package DougDogDoor;
 
+import DougDogDoors.BarkRecognizer;
 import DougDogDoors.DogDoor;
 import DougDogDoors.Remote;
 
 public class DogDoorSimulator {
     public static void main(String[] args) {
         DogDoor door = new DogDoor();
+        BarkRecognizer barkRecognizer = new BarkRecognizer(door);
         Remote remote = new Remote(door);
 
-        System.out.println("Fido barks to go outside...");
-        remote.pressButton();
+        System.out.println("Fido starts barking...");
+        barkRecognizer.recognize("Woof");
         System.out.println("\nFido has gone outside...");
         System.out.println("\nFido's all done...");
 
@@ -20,8 +22,8 @@ public class DogDoorSimulator {
 
         System.out.println("...but he's stuck outside!");
         System.out.println("\nFido starts barking...");
-        System.out.println("...so Gina grabs the remote control.");
-        remote.pressButton();
+        barkRecognizer.recognize("Woof");
+
         System.out.println("\nFido's back inside...");
     }
 }
